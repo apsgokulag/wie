d3bd729236965f06af25bd8d4390a5a1bf3cbecf
Gokul Gopalan <106225510+apsgokulag@users.noreply.github.com>
2025-07-08 15:47:16 +0530

    Delete api-gateway directory

--- api-gateway/src/config/services.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  'auth-service': 'http://localhost:4001', // Replace with actual port for auth-service
-};
--- api-gateway/src/utils/serviceDiscovery.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  getServiceUrl: (serviceName) => {
-    // Return registered service URL dynamically
-    return `http://${serviceName}.localhost`; // Example
-  },
-};
