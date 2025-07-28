d7950b27dd76911a49b6a5da231aff08b56b1773
Gokul Gopalan <106225510+apsgokulag@users.noreply.github.com>
2025-07-28 14:07:08 +0530

    Update routes.jsx

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -14,6 +14,7 @@ import Index from './pages/index'; // Assuming this is the main landing page
 import ForgotPassword from './pages/auth/ForgotPassword';
 import VerifyUser from './pages/auth/VerifyUser';
 import ResetPassword from './pages/auth/ResetPassword';
+import CreateEvent from './pages/ticket/CreateEvent';
 const ProtectedRoute = ({ children, allowedRoles }) => {
   const { token, user } = useSelector((state) => state.auth);
 
@@ -59,6 +60,14 @@ const AppRoutes = () => {
         element={
           <ProtectedRoute allowedRoles={['admin', 'organisation']}>
             <ProfilePage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/ticket/create-event"
+        element={
+          <ProtectedRoute allowedRoles={['organisation', 'admin']}>
+            <CreateEvent />
           </ProtectedRoute>
         }
       />
